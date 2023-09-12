52b02782520295c74e8d45d07c81efcc8f4cd8c7
Sha-Dao <jp2004.oliveira@gmail.com>
2023-09-12 11:41:05 -0300

    1.2

--- mapa.js
+++ mapa.js
@@ -22,22 +22,24 @@ function initMap() {
 
     // Adiciona um manipulador de eventos para capturar informações de rota
     routingControl.on('routesfound', function (e) {
+        console.log(e.routes[0])
         const routes = e.routes;
         const primeiraRota = routes[0]; // Vamos pegar a primeira rota
 
         // Acesse informações como tempo e distância
         const tempo = primeiraRota.summary.totalTime;
-        const distancia = primeiraRota.summary.totalDistance;
+        const distancia = (primeiraRota.summary.totalDistance)
 
         // Exiba as informações na div 'info'
         const infoDiv_1 = document.getElementById('info_1');
         const infoDiv_2 = document.getElementById('info_2');
         const infoDiv_3 = document.getElementById('info_3');
         const infoDiv_4 = document.getElementById('info_4');
+        const tempo1 = (tempo / 60).toFixed(2)
         infoDiv_1.innerHTML = `Distância:`;
         infoDiv_2.innerHTML = `Duração:`;
         infoDiv_3.innerHTML = `${distancia}m`;
-        infoDiv_4.innerHTML = `${tempo / 60}min`;
+        infoDiv_4.innerHTML = `${tempo1}min`;
         //faz a div com o conteúdo aparecer na tela
         var mostrarDivInfo = document.getElementById('info_divEstrutura');
         mostrarDivInfo.style.display = 'block';
@@ -71,6 +73,8 @@ function geocodificarEndereco(endereco, callback) {
                 const resultado = data[0];
                 const latitude = resultado.lat;
                 const longitude = resultado.lon;
+                console.log(latitude, longitude)
+                console.log(resultado)
                 callback([latitude, longitude]);
             } else {
                 alert('Nenhum resultado encontrado para o endereço de destino: ' + endereco);
@@ -109,115 +113,25 @@ async function setVagas(map) {
 
         vagas.forEach(vaga => {
             const { latitude, longitude, id } = vaga; 
-            console.log(latitude, longitude);
             const marker = L.marker([latitude, longitude]).addTo(map);
         
-<<<<<<< Updated upstream
             marker.bindPopup(`ID da Vaga: ${id}`);
         });
     } catch (error) {
         console.error("Erro ao buscar vagas:", error);
     }
 }
-=======
-        
-        function initMap() {
-            // Cria um mapa Leaflet no elemento 'map' com zoom máximo
-            map = L.map('map').setView(pontoPartida, 19);
->>>>>>> Stashed changes
+
+const destino = document.getElementById('destino');
+
+destino.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        calcularRota();
+    }
+});
 
 
-<<<<<<< Updated upstream
 // Inicializa o mapa quando a página carrega
 window.onload = function () {
     initMap();
 };
-=======
-            // Inicializa o controle de roteamento
-            routingControl = L.Routing.control({
-                waypoints: [],
-                routeWhileDragging: true,
-                show: false // Impede a exibição automática de pop-ups
-            }).addTo(map);
-
-            // Adiciona um manipulador de eventos para capturar informações de rota
-            routingControl.on('routesfound', function (e) {
-                const routes = e.routes;
-                const primeiraRota = routes[0]; // Vamos pegar a primeira rota
-
-                // Acesse informações como tempo e distância
-                const tempo = primeiraRota.summary.totalTime;
-                const distancia = primeiraRota.summary.totalDistance;
-
-                // Exiba as informações na div 'info'
-                const infoDiv_1 = document.getElementById('info_1');
-                const infoDiv_2 = document.getElementById('info_2');
-                const infoDiv_3 = document.getElementById('info_3');
-                const infoDiv_4 = document.getElementById('info_4');
-                infoDiv_1.innerHTML = `Distância:`;
-                infoDiv_2.innerHTML = `Duração:`;
-                infoDiv_3.innerHTML = `${distancia}m`;
-                infoDiv_4.innerHTML = `${tempo/60}min`;
-                //faz a div com o conteúdo aparecer na tela
-                var mostrarDivInfo = document.getElementById('info_divEstrutura');
-                mostrarDivInfo.style.display = 'block';
-            });
-        }
-
-        function calcularRota() {
-            
-
-            // Obter o endereço de destino do formulário
-            const destino = document.getElementById('destino').value;
-
-            // Geocodificar o endereço de destino
-            geocodificarEndereco(destino, function (destinoCoords) {
-                // Configurar os pontos de partida (estático) e destino para o controle de roteamento
-                routingControl.setWaypoints([pontoPartida, destinoCoords]);
-
-                // Calcular a rota
-                routingControl.route();
-            });
-        }
-
-        function geocodificarEndereco(endereco, callback) {
-            // Montar a URL da API
-            const api_key = 'pk.637c496315cae65646ec465d593bf88e';
-            const url = `https://us1.locationiq.com/v1/search.php?key=${api_key}&q=${endereco}&format=json`;
-
-            // Fazer a solicitação à API
-            fetch(url)
-                .then(response => response.json())
-                .then(data => {
-                    if (data.length > 0) {
-                        const resultado = data[0];
-                        const latitude = resultado.lat;
-                        const longitude = resultado.lon;
-                        callback([latitude, longitude]);
-                    } else {
-                        alert('Nenhum resultado encontrado para o endereço de destino: ' + endereco);
-                    }
-                })
-                .catch(error => {
-                    console.error('Ocorreu um erro:', error);
-                });
-        }
-
-        // Inicializa o mapa quando a página carrega
-        window.onload = function () {
-            initMap();
-        };
-        
-        const destino = document.getElementById('destino');
-
-        destino.addEventListener("keydown", function(event) {
-            if (event.keyCode === 13) {
-                calcularRota();
-            }
-        });
-
-
-
-
-
->>>>>>> Stashed changes
